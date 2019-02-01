836302e3514472f66dc7457d97182bc7622658b4
Greyson Wright <greysonmwright@gmail.com>
2019-02-01 15:12:51 -0600

    - added method to map properties from an object into sql parameters

--- Query.cs
+++ Query.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.Data;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
-namespace AttendanceTracker_Web.Models.DB
+namespace Query
 {
     public class Query
     {
@@ -22,8 +24,55 @@ namespace AttendanceTracker_Web.Models.DB
 
         public void AddParameter(string parameterName, object value)
         {
-            var parameter = new SqlParameter(parameterName, value);
+            object paramVal = value ?? DBNull.Value;
+            var parameter = new SqlParameter(parameterName, paramVal);
             parameters.Add(parameter);
+        }
+
+        public void AddParameters<T>(T obj)
+        {
+            var validParameters = GetParametersFromQueryString();
+            var objType = typeof(T);
+            foreach (var parameter in validParameters)
+            {
+                var property = objType.GetProperty(parameter, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (property != null)
+                {
+                    var propertyValue = property.GetValue(obj);
+                    var parameterName = string.Format("@{0}", property.Name);
+                    AddParameter(parameterName, propertyValue);
+                }
+            }
+        }
+
+        private List<string> GetParametersFromQueryString()
+        {
+            var tokenizedQueryString = TokenizeQueryString();
+            var parameters = new List<string>();
+            for (int i = 1; i < tokenizedQueryString.Length; i++)
+            {
+                var token = tokenizedQueryString[i];
+                if (!string.IsNullOrWhiteSpace(token))
+                {
+                    var parameter = SanatizeString(token);
+                    parameters.Add(parameter);
+                }
+            }
+            return parameters;
+        }
+
+        private string[] TokenizeQueryString()
+        {
+            char[] delimeters = { '@', ','};
+            var splitQueryString = queryString.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+            return splitQueryString;
+        }
+
+        private string SanatizeString(string str)
+        {
+            char[] trimChars = {' ', ';'};
+            var sanatizedString = str.Trim(trimChars);
+            return sanatizedString;
         }
 
         public DataTable Execute()
